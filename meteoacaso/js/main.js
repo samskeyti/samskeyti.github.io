7e73d23b1af9684e92e9d921bb5fc18cf9644c3f
Daniele Paviani <daniele.paviani@handf.it>
2014-07-29 18:01:47 +0200

    piccole correzioni

--- meteoacaso/js/main.js
+++ meteoacaso/js/main.js
@@ -10,9 +10,10 @@ moment.lang('it',{
 });
 angular.module('App', [])
   .controller('AppController', ['$scope', '$window', function($scope, $window) {
-      
+    
+    $scope.baseT = {sp: 20, su: 30, au: 15, wi: 5};
     $scope.conditions = [
-        {icon: 'day-cloudy', label: 'Parzialmente Nuvoloso', tMod: 0, class: 'sunny'},
+        {icon: 'day-cloudy', label: 'Parzialmente Nuvoloso', tMod: 0, class: 'sunny', weight: {sp: 0, su: 0, au: 0, wi: 0}},
         {icon: 'cloudy', label: 'Nuvoloso', tMod: -3},
         {icon: 'snow', label: 'Neve', tMax: 2, tMin: -4}
     ];
@@ -20,10 +21,11 @@ angular.module('App', [])
     $scope.days = function(){
         var days = [];
         var randomDay = function(delta){
+            var d = new Date().getTime() + 86400*1000*delta;
             var c = $scope.conditions[Math.floor(Math.random()*$scope.conditions.length)];
             var t = Math.floor(Math.random()*30);
             var day = {
-                date: $window.moment(new Date().getTime() + 86400*1000*delta).calendar(),
+                date: $window.moment(d).calendar(),
                 condition: c,
                 temperature: t
             };
