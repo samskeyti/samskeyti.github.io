ae35d5d4797f3cf58f5fe9ec69ce8d1e1e33e887
Daniele Paviani <daniele.paviani@handf.it>
2014-07-30 14:16:20 +0200

    rifiniture

--- meteoacaso/js/main.js
+++ meteoacaso/js/main.js
@@ -13,17 +13,19 @@ angular.module('App', [])
     
     
     $scope.conditions = [
-        {icon: 'day-cloudy', label: 'Parzialmente Nuvoloso', tMod: 0, class: 'partcloudy', weight: '553446'},
-        {icon: 'cloudy', label: 'Nuvoloso', tMod: -3, class: 'cloudy',weight:'663157'},
-        {icon: 'snow', label: 'Neve', tMod:0, tMax: 2, tMin: -4, class: 'snowy', weight:'310002'},
-        {icon: 'hot', label: 'Caldo afoso', tMod: 7, class: 'hot', weight: '002710'},
-        {icon: 'day-rain', label: 'Variabile', tMod: 0, class: 'variable', weight: '684264'},
-        {icon: 'fog', label: 'Nebbia', tMod:-5, class: 'foggy', weight: '310025'},
-        {icon: 'day-sunny', label: 'Sereno', tMod: 3, class: 'sunny', weight: '468963'},
-        {icon: 'rain', label: 'Pioggia', tMod: -2, class: 'rainy', weight: '552256'},
-        {icon: 'thunderstorm', label: 'Temporali', tMod:-4, class: 'thunder', weight: '123321'},
+        {icon: 'day-cloudy', label: 'Parzialmente Nuvoloso', tMod: 0, class: 'partcloudy', weight: '775668'},
+        {icon: 'cloudy', label: 'Nuvoloso', tMod: -3, class: 'cloudy',weight:'885379'},
+        {icon: 'snow', label: 'Neve', tMod:0, tMax: 2, tMin: -4, class: 'snowy', weight:'520004'},
+        {icon: 'hot', label: 'Caldo afoso', tMod: 5, class: 'hot', weight: '004920'},
+        {icon: 'day-rain', label: 'Variabile', tMod: 0, class: 'variable', weight: '795375'},
+        {icon: 'fog', label: 'Nebbia', tMod:-5, class: 'foggy', weight: '530047'},
+        {icon: 'day-sunny', label: 'Sereno', tMod: 3, class: 'sunny', weight: '579974'},
+        {icon: 'rain', label: 'Pioggia', tMod: -2, class: 'rainy', weight: '663367'},
+        {icon: 'thunderstorm', label: 'Temporali', tMod:-4, class: 'thunder', weight: '235532'},
         
-        {icon: 'meteor', label: 'Pioggia di meteore', tMod: 30, class: 'meteor', weight: '000100'}
+        {icon: 'meteor', label: 'Pioggia di meteore', tMod: 30, class: 'meteor', weight: '111111'},
+        {icon: 'alien', label: 'Atterraggio alieni', tMod: 0, class: 'alien', weight: '111111'},
+        {icon: 'tornado', label: 'Tornado', tMod: 0, class: 'tornado', weight: '111111'}
     ];
     $scope.baseT = [5,15,23,27,20,10];
       
